6212c9ec9773258ded265093b9fc1c3a47ba380a
xuruiyao <xurui_yao@163.com>
2024-04-23 13:07:03 +0800

    office debug task handler

--- packages/vscode-extension/src/extension.ts
+++ packages/vscode-extension/src/extension.ts
@@ -98,6 +98,7 @@ import { loadLocalizedStrings } from "./utils/localizeUtils";
 import { checkProjectTypeAndSendTelemetry } from "./utils/projectChecker";
 import { ReleaseNote } from "./utils/releaseNote";
 import { ExtensionSurvey } from "./utils/survey";
+import { registerOfficeTaskAndDebugEvents } from "./debug/officeTaskHandler";
 
 export let VS_CODE_UI: VsCodeUI;
 
@@ -230,7 +231,7 @@ function activateOfficeDevRegistration(context: vscode.ExtensionContext) {
   }
 
   // Register task and debug event handlers, as well as sending telemetries
-  registerTeamsfxTaskAndDebugEvents();
+  registerOfficeTaskAndDebugEvents();
 }
 
 /**
